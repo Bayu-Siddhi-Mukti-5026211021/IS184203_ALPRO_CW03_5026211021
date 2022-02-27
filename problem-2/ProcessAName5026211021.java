7beba2194a3c63e82a5b0d60b860f3b6a9d9b5ea
Bayu Siddhi Mukti <100100208+Bayu-Siddhi-Mukti-5026211021@users.noreply.github.com>
2022-02-27 20:14:07 +0700

    Merapikan syntax

--- problem-2/ProcessAName5026211021.java
+++ problem-2/ProcessAName5026211021.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class ProcessAName5026211021 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Type your name: ");
-        String fullName = sc.nextLine();
-        String processName = processName(fullName);
-        System.out.println("Your name is: " + processName);
+    	public static void main(String[] args) {
+        	Scanner sc = new Scanner(System.in);
+        	System.out.print("Type your name: ");
+        	String fullName = sc.nextLine();
+        	String processName = processName(fullName);
+       		System.out.println("Your name is: " + processName);
 	}
 
-    public static String processName(String fullName) {
+    	public static String processName(String fullName) {
  		String[] arrayName = fullName.split(" ");
-        String processName = arrayName[arrayName.length - 1] + ",";
-        for (int i = 0; i < arrayName.length - 1; i++) {
-            processName = processName + " " + arrayName[i].charAt(0) + ".";
-        }
-        return processName;
+        	String processName = arrayName[arrayName.length - 1] + ",";
+        	for (int i = 0; i < arrayName.length - 1; i++) {
+           	processName = processName + " " + arrayName[i].charAt(0) + ".";
+        	}
+        	return processName;
    	}
 }
