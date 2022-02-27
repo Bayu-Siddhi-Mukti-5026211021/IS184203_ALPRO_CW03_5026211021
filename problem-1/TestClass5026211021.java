985c0d41ff34b927b768a9f9dd4fcd6237a9ed0a
Bayu Siddhi Mukti <100100208+Bayu-Siddhi-Mukti-5026211021@users.noreply.github.com>
2022-02-27 14:28:27 +0700

    Print input dan output

--- problem-1/TestClass5026211021.java
+++ problem-1/TestClass5026211021.java
@@ -5,18 +5,18 @@ public class TestClass5026211021 {
 		Scanner sc = new Scanner(System.in);
 		ComputeMethods5026211021 method = new ComputeMethods5026211021();
 
-		System.out.print("Temperature value from fahrenheit = ");
+		System.out.println("INPUT");
+		System.out.print("Temperature value in fahrenheit = ");
 		double fahrenheitValue = sc.nextDouble();
 		double celciusValue = method.fToC(fahrenheitValue);
 		
-		System.out.print("Value of the opposite side of the triangle (A) = ");
+		System.out.print("Value of the opposite side (A) of the triangle = ");
 		double a = sc.nextDouble();
-		System.out.print("Value of the adjacent side of the triangle (B) = ");
+		System.out.print("Value of the adjacent side (B) of the triangle = ");
 		double b = sc.nextDouble();
 		double c = method.hypotenuse(a, b);
 
-		System.out.println();
-		
+		System.out.println("\nOUTPUT");
 		System.out.println("Temp in celcius is " + celciusValue);
 		System.out.println("Hypotenuse (C) is " + c);
 		System.out.println("The sum of the dice values is " + method.roll());
